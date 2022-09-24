8869fec6f748a22477031b616fb3669c9b126303
Magbillionz <111141227+Magbillionz@users.noreply.github.com>
2022-09-24 09:57:46 +0100

    Initial commit

---
+++ src/components/todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Brush Teeth",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Dress Girls",
+    completed: true
+  },
+  {
+    id: 3,
+    text: "Feed Girls",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "Feed Cats",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Go To Work",
+    completed: true
+  },
+  {
+    id: 6,
+    text: "Pick up groceries",
+    completed: true
+  }
+];
+
+export default todosData;
